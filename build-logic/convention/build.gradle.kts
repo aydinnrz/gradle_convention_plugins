82737fdb5b8718c8cb60cffd585dcc6aa2913ca5
Aydin Norouzi <a.norouzi@enbw.com>
2023-09-11 23:04:23 +0300

    add build-logic with settings and build gradle files. include it as build in main projects pluginManagement.

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,20 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+     implementation(libs.android.gradle.plugin)
+     implementation(libs.kotlin.gradle.plugin)
+}
